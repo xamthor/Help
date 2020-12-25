c68da8004a08696489ff59283c2bc4be16f92644
Smiley <jcsmileyjr@gmail.com>
2020-12-25 07:54:54 -0600

    Fixed mis-spell folder name

--- Client/Help/src/app/app.module.ts
+++ Client/Help/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginScreenComponent } from './components/login-screen/login-screen.component';
-import { LoginInputsComponent } from './componens/login-inputs/login-inputs.component';
+import { LoginInputsComponent } from './components/login-inputs/login-inputs.component';
 
 @NgModule({
   declarations: [
