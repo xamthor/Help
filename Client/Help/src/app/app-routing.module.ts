596a085a2cda0a8470b000baa155e9bb131bff3b
Smiley <jcsmileyjr@gmail.com>
2020-12-26 09:06:22 -0600

    Feature: setup basic routing between Login screen and Setup screens

--- Client/Help/src/app/app-routing.module.ts
+++ Client/Help/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginScreenComponent} from './components/login-screen/login-screen.component';
+import {SetupNameComponent} from './components/setup-name/setup-name.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login-screen', component: LoginScreenComponent },
+  { path: 'setup-screen', component: SetupNameComponent },
+  { path: '',   redirectTo: '/login-screen', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
