8d030da6f02fa357ef0a9622451b354a0970248d
Smiley <jcsmileyjr@gmail.com>
2020-12-30 07:24:41 -0600

    Refactor and restyle message content for dynamic content

--- Client/Help/src/app/components/content-messages-gallery/content-messages-gallery.component.ts
+++ Client/Help/src/app/components/content-messages-gallery/content-messages-gallery.component.ts
@@ -8,6 +8,13 @@ import {faUserCircle } from '@fortawesome/free-solid-svg-icons';
 })
 export class ContentMessagesGalleryComponent implements OnInit {
   profilePic = faUserCircle;
+  arrayOfProfiles = [
+    {name: "Jen Turner", lastMessageTime:"23"},
+    {name: "Jake Stafford", lastMessageTime:"26"},
+    {name: "Katie Drake", lastMessageTime:"32"},
+    {name: "Joe McCarty", lastMessageTime:"40"},
+    {name: "Charlie Patterson", lastMessageTime:"48"},
+  ];
 
   constructor() { }
 
