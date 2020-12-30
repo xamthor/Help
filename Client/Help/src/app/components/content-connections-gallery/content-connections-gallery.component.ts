b43a75dd273a6e9bb4b286bf3e3990977e506e99
Smiley <jcsmileyjr@gmail.com>
2020-12-30 12:39:43 -0600

    Refactor to integrate data from a service to connections content area

--- Client/Help/src/app/components/content-connections-gallery/content-connections-gallery.component.ts
+++ Client/Help/src/app/components/content-connections-gallery/content-connections-gallery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import {UserAccountService} from '../../services/user-account.service';
 
 @Component({
   selector: 'app-content-connections-gallery',
@@ -8,23 +9,9 @@ import {faUserCircle } from '@fortawesome/free-solid-svg-icons';
 })
 export class ContentConnectionsGalleryComponent implements OnInit {
   profilePic = faUserCircle;
-  arrayOfProfiles = [
-    {name: "Jen Turner", lastMessageTime:"23"},
-    {name: "Jake Stafford", lastMessageTime:"26"},
-    {name: "Katie Drake", lastMessageTime:"32"},
-    {name: "Joe McCarty", lastMessageTime:"40"},
-    {name: "Charlie Patterson", lastMessageTime:"48"},
-    {name: "James Bond", lastMessageTime:"48"},
-    {name: "Daisy Watson", lastMessageTime:"49"},
-    {name: "Tommy Drake", lastMessageTime:"48"},
-    {name: "Bacari White", lastMessageTime:"48"},
-    {name: "Maranida Watson", lastMessageTime:"48"},
-    {name: "Nate Stokes", lastMessageTime:"48"},
-    {name: "Sean Wilson", lastMessageTime:"48"},
-    {name: "Paul Malon", lastMessageTime:"48"},
-  ];
+  arrayOfProfiles = this.userAccountService.getConnections();
 
-  constructor() { }
+  constructor(private userAccountService: UserAccountService) { }
 
   ngOnInit(): void {
   }
