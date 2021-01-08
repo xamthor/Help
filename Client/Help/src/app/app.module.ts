60c4102de224114ff7cfed4228f50b91f42ab311
Smiley <jcsmileyjr@gmail.com>
2021-01-08 07:50:54 -0600

    WIP: add cookie service to set cookie to fix auth issue

--- Client/Help/src/app/app.module.ts
+++ Client/Help/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from  '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,7 +49,7 @@ import { MenuScreenComponent } from './components/menu-screen/menu-screen.compon
     AppRoutingModule,
     FontAwesomeModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Client/Help/src/app/services/status.service.ts
+++ Client/Help/src/app/services/status.service.ts
@@ -2,21 +2,23 @@ import { Injectable } from '@angular/core';
 import {AuthenticateService} from '../services/authenticate.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Router} from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatusService {
 
-  constructor(private http: HttpClient, private router:Router, private authenticateUser: AuthenticateService) { }
+  constructor(private http: HttpClient, private router:Router, private authenticateUser: AuthenticateService, private cookieService: CookieService) { }
 
   async updateStatus(userContent: string){
     const authToken = this.authenticateUser.getAuthToken();
     console.log(authToken);
-    const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8'});
-    let options = { headers: headers, withCredentials: true };
+    this.cookieService.set('Set-Cookie',authToken);
+    const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8', 'Authorization': authToken});
+    //let options = { headers: headers, withCredentials: true };
     var raw = JSON.stringify({"content":userContent});
-    await this.http.post<any>('http://localhost:3000/status/create', raw, options).subscribe(
+    await this.http.post<any>('http://localhost:3000/status/create', raw, {headers}).subscribe(
       results => {
 
         console.log(results);
