b463b78fcd2a34c73b2f03156f87de8cc308acd3
xam <admin@xbitestudio.com>
2021-01-09 02:26:37 -0600

    Status Model includes users info

--- Server/src/models/status.model.js
+++ Server/src/models/status.model.js
@@ -6,8 +6,9 @@ config();
 
 const statusSchema = new mongoose.Schema({
 
-  user_id: {
-    type: String,
+  user_id: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref:'User', required:true 
   },
   content: {
     type: String,
