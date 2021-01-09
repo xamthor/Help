a1a4c6eaceb573b302790bdbf37f32a4297074ea
Smiley <jcsmileyjr@gmail.com>
2021-01-09 06:48:47 -0600

    WIP: Integrating search API for search connection screen

--- Client/Help/src/app/components/search-connections/search-connections.component.ts
+++ Client/Help/src/app/components/search-connections/search-connections.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {ConnectionsService} from '../../services/connections.service';
+//import { User } from '../../interfaces/user';
+import {faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-connections',
@@ -7,18 +10,49 @@ import {Router} from '@angular/router';
   styleUrls: ['./search-connections.component.css']
 })
 export class SearchConnectionsComponent implements OnInit {
+  profilePic = faUserCircle;
   titleSearch: string = "Search"; // Input field title and sets the icon in the input field
   query: string = ""; // variable updated from the input designiated input field and then used to update the temp User
+  profiles: any = [
+    {
+      phoneNumber: "111--111-2222",
+      firstName: "Bob",
+      lastName: "Smith",
+      _id: "100",
+      email: "bobSmith@google.com",
+      userName: "bSmith",
+      __v: 0
+    },
+    {
+      phoneNumber: "222-222-2222",
+      firstName: "Jane",
+      lastName: "Doe",
+      _id: "200",
+      email: "janeSmith@google.com",
+      userName: "jSmith",
+      __v: 0
+    }
+  ]; // TESTING
+  arrayOfProfiles = this.profiles; // TESTING
+  //arrayOfProfiles :any[] = []
 
-  constructor( private router:Router) { }
+  constructor( private router:Router, private connectionService: ConnectionsService) { }
 
   ngOnInit(): void {
   }
 
   // Capture user input from input field
-  findUser($event:any){
+  getUser($event:any){
     this.query = $event;
   }  
+
+  // Search database for users
+  findUser(){
+    this.connectionService.searchConnections(this.query).subscribe(data => {
+      console.log(data);
+      this.arrayOfProfiles = data;
+    })
+  } 
 
   finish(){
     this.router.navigate(['/feed']);
---
+++ Client/Help/src/app/services/connections.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {AuthenticateService} from '../services/authenticate.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConnectionsService {
+
+  constructor(private http: HttpClient, private authenticateUser: AuthenticateService, private cookieService: CookieService) { }
+
+  searchConnections(searchTerm:string){
+    const authToken = this.authenticateUser.getAuthToken();
+    this.cookieService.set('jwt',authToken);
+    const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8', 'Authorization': authToken});
+    let options = { headers: headers, withCredentials: true };
+    return this.http.get<any>(`http://localhost:3000/search?s=${searchTerm}`, options);    
+  }
+}
