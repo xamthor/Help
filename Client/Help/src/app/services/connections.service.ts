17362326c7e5094144005b1969aa6b086e8ec039
Smiley <jcsmileyjr@gmail.com>
2021-01-09 06:58:01 -0600

    Refactor back-end server to allow search API call

--- Client/Help/src/app/services/connections.service.ts
+++ Client/Help/src/app/services/connections.service.ts
@@ -13,7 +13,7 @@ export class ConnectionsService {
   searchConnections(searchTerm:string){
     const authToken = this.authenticateUser.getAuthToken();
     this.cookieService.set('jwt',authToken);
-    const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8', 'Authorization': authToken});
+    const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8'});
     let options = { headers: headers, withCredentials: true };
     return this.http.get<any>(`http://localhost:3000/search?s=${searchTerm}`, options);    
   }
--- Server/src/routes/search.route.js
+++ Server/src/routes/search.route.js
@@ -2,12 +2,23 @@ import { Router } from "express";
 import searchController from "../controllers/search.controller";
 import catchAsync from "../middleware/catchAsync";
 import authentication from "../middleware/authenticate";
+import cors from "cors";
+
+const corsOptions = {
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+}
+
+
 
 const { search } = searchController;
 const { authenticate } = authentication;
 
 const searchRouter = Router();
-
+searchRouter.use(cors(corsOptions));
 searchRouter.get("/search", authenticate, catchAsync(search));
 
 export default searchRouter;
