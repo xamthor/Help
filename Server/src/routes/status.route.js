ba7f101ff3063aa092f4857c55eeb59342e4ca82
xam <admin@xbitestudio.com>
2021-01-09 00:19:47 -0600

    fix cors syntax in status route

--- Server/src/routes/status.route.js
+++ Server/src/routes/status.route.js
@@ -2,20 +2,21 @@ import { Router } from "express";
 import statusController from "../controllers/status.controller";
 import catchAsync from "../middleware/catchAsync";
 import authentication from "../middleware/authenticate";
-var cors = require('cors')
+import cors from "cors";
 
 const { create, latest, all } = statusController;
 const { authenticate } = authentication;
 
 const testRouter = Router();
 
-var corsOptions = {
+const corsOptions = {
     origin: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
     credentials: true,
 }
+
 testRouter.use(cors(corsOptions))
 
 testRouter.get("/all", authenticate, catchAsync(all));
